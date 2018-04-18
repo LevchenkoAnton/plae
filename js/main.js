a1c3d27b04c9404e26442b4dfba262488f3e1def
Anton Levchenko <levchik03021998@gmail.com>
2018-04-18 13:34:38 +0300

    range section was done

--- js/main.js
+++ js/main.js
@@ -2,5 +2,10 @@ $(document).ready(function(){
     $('.header__slider').slick({
         dots: true,
         arrows: false
-});
+    });
+
+    $('.range__slider').slick({
+        arrows: true,
+        fade: true
+    });
 });
