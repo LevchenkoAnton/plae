4baef04feb93fa6874867cb960f8bc6cc9bc468c
Anton Levchenko <levchik03021998@gmail.com>
2018-04-19 13:48:45 +0300

    optimized css and img files

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
+
+    var emailInput = document.querySelector('.registration__input');
+    var form = document.querySelector('.registration__form');
+    form.addEventListener('submit', function (evt) {
+        if(!emailInput.value) {
+            evt.preventDefault();
+            $('.registration__input').attr('placeholder', 'You must fill this field');
+        }
+    });
+
     $('.header__slider').slick({
         dots: true,
-        arrows: false
+        arrows: false,
+        autoplay: true
     });
 
     $('.range__slider').slick({
         arrows: true,
-        fade: true
+        fade: true,
+        autoplay: true
     });
 });
